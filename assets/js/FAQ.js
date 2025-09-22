7a29afe55006852ddede462800a17134081b53ad
tomMckechnie-90 <tommckechnie8@gmail.com>
2025-09-22 14:20:59 +0100

    Refactor FAQ functionality to remove close button and simplify answer toggling

--- assets/js/FAQ.js
+++ assets/js/FAQ.js
@@ -1,33 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const questions = document.querySelectorAll('.question');
     const answers = document.querySelectorAll('.answer');
-    const closeAnswerBtn = document.querySelectorAll('.close-answer');
 
-    // When a question is clicked
     questions.forEach(function (question, i) {
         question.addEventListener('click', function () {
-            // This will close the current open answer when a new one is clicked.
-            answers.forEach(function (ans) {
-                ans.classList.add('hidden');
-            });
-            closeAnswerBtn.forEach(function (btn) {
-                btn.classList.add('hidden');
-            });
-
-            // Show the answer and close button for the question that has been clicked.
-            answers[i].classList.remove('hidden');
-            closeAnswerBtn[i].classList.remove('hidden');
-        });
-    });
-    // When a close button is clicked
-    closeAnswerBtn.forEach(function (btn, i) {
-        btn.addEventListener('click', function (event) {
-            // This will stop the click opening the answer again
-            event.stopPropagation();
-
-            // This will then hide the answers and close buttons
-            answers[i].classList.add('hidden');
-            btn.classList.add('hidden');
-        });
-    });
+            // If the answer is open, close it
+            if (!answers[i].classList.contains('hidden')) {
+                answers[i].classList.add('hidden');
+            } else {
+                // If not close all the answers then open the one that is clicked
+                answers.forEach(ans => ans.classList.add('hidden'));
+                answers[i].classList.remove('hidden');
+            }
+        })
+    })
 });
