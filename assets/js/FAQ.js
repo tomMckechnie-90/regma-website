2bdb9ffc5dc5fba05dd4a9704334de95a9c839a2
tomMckechnie-90 <tommckechnie8@gmail.com>
2025-09-22 15:02:14 +0100

    Enhance FAQ functionality with improved answer visibility and transition effects

--- assets/js/FAQ.js
+++ assets/js/FAQ.js
@@ -6,11 +6,27 @@ document.addEventListener('DOMContentLoaded', function () {
         question.addEventListener('click', function () {
             // If the answer is open, close it
             if (!answers[i].classList.contains('hidden')) {
-                answers[i].classList.add('hidden');
+                answers[i].classList.remove('opacity-100');
+                answers[i].classList.add('opacity-0');
+                setTimeout(() => {
+                    answers[i].classList.add('hidden');
+                }, 500);
             } else {
                 // If not close all the answers then open the one that is clicked
-                answers.forEach(ans => ans.classList.add('hidden'));
+                answers.forEach((ans, idx) => {
+                    if (idx !== i) {
+                        ans.classList.remove('opacity-100');
+                        ans.classList.add('opacity-0');
+                        ans.classList.add('hidden');
+                    }
+                });
+
                 answers[i].classList.remove('hidden');
+                // show and fade in the clicked answer.
+                setTimeout(() => {
+                    answers[i].classList.remove('opacity-0');
+                    answers[i].classList.add('opacity-100');
+                }, 10);
             }
         })
     })
